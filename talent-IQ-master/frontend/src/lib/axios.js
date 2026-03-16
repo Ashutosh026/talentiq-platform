a19b6b8b63bd43b4467e4db87afb406f86d4b9a6
Ashutosh026 <ashutoshtiwarioffc@gmail.com>
2026-03-16 22:46:22 +0530

    Add Clerk token interceptor to axios instance for authorization handling

--- talent-IQ-master/frontend/src/lib/axios.js
+++ talent-IQ-master/frontend/src/lib/axios.js
@@ -1,8 +1,26 @@
 import axios from "axios";
+import { getAuth } from "@clerk/clerk-react";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
 });
+
+// Add Clerk token interceptor - THIS IS CRITICAL
+axiosInstance.interceptors.request.use(
+  async (config) => {
+    try {
+      const { getToken } = getAuth();
+      const token = await getToken();
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+    } catch (error) {
+      console.error("Failed to get Clerk token:", error);
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
 
 export default axiosInstance;
